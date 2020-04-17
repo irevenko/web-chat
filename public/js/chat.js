1d0bc49ca52c8aef6b0a558ca7fef507dcbc57b0
irevenko <kekalkaru20@gmail.com>
2020-04-17 16:43:33 +0300

    Refactor chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,43 +1,30 @@
 const socket = io.connect('http://localhost:3000');
 
-const username = document.getElementById('username-input');
 const message = document.getElementById('message-input');
 const sendMsg = document.getElementById('send-message');
 const displayMsg = document.getElementById('display-message');
 const typingLabel = document.getElementById('typing-label');
 const chatWindow = document.getElementById('chat-window');
 const usersCounter = document.getElementById('users-counter');
-const userErr = document.getElementById('username-error');
 const msgErr = document.getElementById('message-error');
+const join = document.getElementById('you-joined');
 
-
-sendMsg.addEventListener('click', () => { 
-  if (username.value === '' || username.value == null) {
-    userErr.innerHTML = '🚨Username is required!';
-    return;
-  }
-  if (message.value === '' || message.value == null) {
-    msgErr.innerHTML = '🚨Message is required!';
-    return;
-  }
-
-  socket.emit('new-message', {
-    message: message.value,
-    username: username.value
-  });
-  message.value = '';
-  userErr.innerHTML = '';
-  msgErr.innerHTML = '';
-});
-
-message.addEventListener('keypress', () => {
-  socket.emit('is-typing', username.value);
-});
+const username = prompt('Enter your Username');
+join.innerHTML = '<p>You have joined the chat!<p>';
+socket.emit('new-user', username);
 
 socket.on('new-message', (data) => {
   typingLabel.innerHTML = '';
   displayMsg.innerHTML += `<p><strong>${data.username}</strong> <em>at ${new Date().getHours()}:${new Date().getMinutes()}</em> : ${data.message}</p>`;
   chatWindow.scrollTop = chatWindow.scrollHeight;
+});
+
+socket.on('user-connected', (username) => {
+  appendMessage(`<p><strong>${username}</strong> has connected!</p>`);
+});
+
+socket.on('user-disconnected', (username) => {
+  appendMessage(`<p><strong>${username}</strong> has disconnected!</p>`);
 });
 
 socket.on('is-typing', (data) => {
@@ -48,3 +35,27 @@ socket.on('is-typing', (data) => {
 socket.on('broadcast', (data) => { 
   usersCounter.innerHTML = data;
 });
+
+sendMsg.addEventListener('click', () => { 
+  if (message.value === '' || message.value == null) {
+    msgErr.innerHTML = '🚨Message is required!';
+    return;
+  }
+
+  socket.emit('new-message', {
+    message: message.value,
+    username: username
+  });
+  message.value = '';
+  msgErr.innerHTML = '';
+});
+
+message.addEventListener('keypress', () => {
+  socket.emit('is-typing', username);
+});
+
+function appendMessage(msgText) {
+  const msgElement = document.createElement('span')
+  msgElement.innerHTML = msgText
+  displayMsg.append(msgElement)
+}
--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const io = socket(server);
 
 const PID = process.pid;
 const PORT = process.env.PORT || 3000;
+const users = {};
 let usersNum = 0; 
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,6 +17,11 @@ io.on('connection', (socket) => {
   console.log(`The socket is connected! Socket id: ${socket.id}`);
   usersNum++;
   io.emit('broadcast', `Online: ${usersNum}`);
+
+  socket.on('new-user', (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit('user-connected', username);
+  })
 
   socket.on('new-message', (data) => {
     io.emit('new-message', data);
@@ -27,6 +34,8 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     usersNum--;
     io.emit('broadcast', `Online: ${usersNum}`);
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
   });
 
 });
